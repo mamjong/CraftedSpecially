25e16acfdd8b53f3e34989ec79068c325301f102
Mark de Jong <{ID}+{username}@users.noreply.github.com>
2023-09-21 17:13:45 +0200

    Add manual instrumentation to AndroidAPI

--- Services/AndroidApi/AndroidApi/Program.cs
+++ Services/AndroidApi/AndroidApi/Program.cs
@@ -1,4 +1,10 @@
 using CraftedSpecially.AndroidApi.Infrastructure;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +16,66 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Configure OpenTelemetry
+
+// Build a resource configuration action to set service information.
+Action<ResourceBuilder> configureResource = resourceBuilder => resourceBuilder.AddService(
+    serviceName: builder.Configuration.GetValue("ServiceName", defaultValue: "Catalog")!,
+    serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "Unknown",
+    serviceInstanceId: Environment.MachineName);
+
+// Build a configuration action to set the OTLP exporter endpoint.
+Action<OtlpExporterOptions> configureOtlpExporter = options =>
+    options.Endpoint = new Uri(builder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
+
+const string activitySourceName = "AndroidApi";
+const string meterName = "AndroidApi";
+
+// Configure tracing and metrics.
+builder.Services
+    .AddOpenTelemetry()
+    .ConfigureResource(configureResource)
+    .WithTracing(tracingBuilder =>
+    {
+        // Use IConfiguration binding for AspNetCore instrumentation options.
+        builder.Services.Configure<AspNetCoreInstrumentationOptions>(
+            builder.Configuration.GetSection("AspNetCoreInstrumentation"));
+
+        tracingBuilder
+            .AddSource(activitySourceName)
+            .SetSampler<AlwaysOnSampler>()
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(configureOtlpExporter);
+    })
+    .WithMetrics(metricsBuilder =>
+    {
+        metricsBuilder
+            .AddMeter(meterName)
+            .AddRuntimeInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(configureOtlpExporter);
+    });
+
+// Clear default logging providers used by WebApplication host.
+builder.Logging.ClearProviders();
+
+// Configure OpenTelemetry Logging.
+builder.Logging.AddOpenTelemetry(options =>
+{
+    // Note: See appsettings.json Logging:OpenTelemetry section for configuration.
+
+    var resourceBuilder = ResourceBuilder.CreateDefault();
+    configureResource(resourceBuilder);
+    options
+        .SetResourceBuilder(resourceBuilder)
+        .AddOtlpExporter(configureOtlpExporter)
+        .AddConsoleExporter();
+});
+
+#endregion
 
 var app = builder.Build();
 
